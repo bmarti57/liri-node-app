5c3ddefd42d32a98aac235511b35f3e99c3ef1f4
Brian Martin <Brian Martin>
2017-08-21 18:24:54 -0400

    twitter, spotify, and omdb all fully functional

--- liri.js
+++ liri.js
@@ -74,7 +74,7 @@ function spotifyThis() {
                 console.log("Artist: " + data.tracks.items[0].artists[0].name);                
                 console.log("Title: " + data.tracks.items[0].name);
                 console.log("Preview URL: " + data.tracks.items[0].preview_url);
-                console.log("Album: " + data.items[0].album.name);                
+                console.log("Album: " + data.items[0].album);                
             });
         } else {    
                 spotify.search({
@@ -85,11 +85,11 @@ function spotifyThis() {
                         console.log('Error occurred: ' + error);
                         return;
                     }
-                    // DO SOMETHING WITH 'data'
+                    
                     console.log("Artist: " + data.tracks.items[0].artists[0].name);                    
                     console.log("Title: " + data.tracks.items[0].name);
                     console.log("Preview URL: " + data.tracks.items[0].preview_url);
-                    console.log("Album: " + data.items[0].album.name);
+                    console.log("Album: " + data.tracks.items[0].album.name);
                 });
             
     
@@ -112,11 +112,11 @@ function omdb() {
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
         } else {
-            request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&tomatoes=true&r=json', function(error, response, body) {
+            request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=40e9cece', function(error, response, body) {
                 console.log("Title: " + JSON.parse(body).Title);   
                 console.log("Release Year: " + JSON.parse(body).Year);
                 console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                //console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
                 console.log("Country: " + JSON.parse(body).Country);
                 console.log("Language: " + JSON.parse(body).Language);
                 console.log("Plot: " + JSON.parse(body).Plot);
